83940e400b2ce05fae14da0010954be61dcadb44
MoonJaeGyeong <bblbblan69@gmail.com>
2024-11-13 11:00:02 +0900

    refactor : 쿼리문 개선

--- src/main/java/com/example/humorie/reservation/repository/CustomReservationRepository.java
+++ src/main/java/com/example/humorie/reservation/repository/CustomReservationRepository.java
@@ -3,8 +3,11 @@ package com.example.humorie.reservation.repository;
 import com.example.humorie.reservation.entity.Reservation;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface CustomReservationRepository {
 
     List<Reservation> findAllByAccountEmail(String email);
+
+    Optional<Reservation> findByReservationUid(String Uid);
 }
--- src/main/java/com/example/humorie/reservation/repository/CustomReservationRepositoryImpl.java
+++ src/main/java/com/example/humorie/reservation/repository/CustomReservationRepositoryImpl.java
@@ -4,9 +4,11 @@ import com.example.humorie.reservation.entity.Reservation;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import java.util.List;
+import java.util.Optional;
 
 import static com.example.humorie.account.entity.QAccountDetail.accountDetail;
 import static com.example.humorie.consultant.counselor.entity.QCounselor.counselor;
+import static com.example.humorie.payment.entity.QPayment.payment;
 import static com.example.humorie.reservation.entity.QReservation.reservation;
 
 @RequiredArgsConstructor
@@ -24,4 +26,16 @@ public class CustomReservationRepositoryImpl implements CustomReservationReposit
                  .orderBy(reservation.createdAt.desc())
                  .fetchJoin().fetch();
     }
+
+    @Override
+    public Optional<Reservation> findByReservationUid(String uid) {
+        return Optional.ofNullable(queryFactory
+                .selectFrom(reservation)
+                .leftJoin(reservation.account, accountDetail).fetchJoin()
+                .leftJoin(reservation.payment, payment).fetchJoin()
+                .leftJoin(reservation.counselor, counselor).fetchJoin()
+                .where(reservation.reservationUid.eq(uid))
+                .fetchOne());
+    }
+
 }
